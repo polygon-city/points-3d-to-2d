cbe2a4751c6f51386681b0b4bdb2cc9e4165b1e5
Robin Hawkes <rob@rawkes.com>
2015-06-26 12:28:48 +0100

    Tweaked collinear detection threshold

--- index.js
+++ index.js
@@ -11,9 +11,33 @@ var sylvester = require("sylvester");
 var points3dto2d = function(points3d, zUP, parent) {
   parent = (!parent) ? {} : parent;
 
-  var p0 = $V(points3d[0]);
-  var p1 = $V(points3d[1]);
-  var p2 = $V(points3d[2]);
+  var p0, p1, p2;
+
+  // TODO: Find a good value for this threshold
+  // - 0.01 still resulted in collinear issues
+  // - 0.05 seems to work well, though unsure if it causes false-positives
+  // TODO: Look at exposing this as an option rather than hard-coding it
+  var collinearThreshold = 0.05;
+
+  // Find first sequence of points that aren't collinear
+  _.each(points3d, function(point, pIndex) {
+    // Exit if no more points are available
+    if (pIndex === points3d.length - 2) {
+      return false;
+    }
+
+    p0 = $V(point);
+    p1 = $V(points3d[pIndex+1]);
+    p2 = $V(points3d[pIndex+2]);
+
+    // Colinear or near-colinear?
+    var cross = p0.subtract(p1).cross(p0.subtract(p2));
+
+    // Exit if non-collinear points are found
+    if (Math.abs(cross.e(1)) > collinearThreshold || Math.abs(cross.e(2)) > collinearThreshold || Math.abs(cross.e(3)) > collinearThreshold) {
+      return false;
+    }
+  });
 
   var locOrigin = (parent.origin) ? parent.origin : p0.dup();
   var locX = (parent.locX) ? parent.locX : p1.subtract(locOrigin);
